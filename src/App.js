ea7da6fe1d7404bd6d1896458303e3393ac127bf
zeeshan_akram <zeeshan.akram@invozone.com>
2021-01-15 09:55:41 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
 function App() {
+  const [html, setHtml] = useState('');
+  const [show, setShow] = useState(false);
+  const handleChange = (e) => {
+    // setHtml(e.target.value);
+    handleSubmit(e.target.value);
+  };
+  const handleSubmit = (data) => {
+    const parser = new DOMParser();
+    const getData = parser.parseFromString(data, 'text/html');
+    const pTag = getData.querySelectorAll('p');
+    pTag.forEach((ele) => {
+      console.log(ele.innerHTML);
+    });
+    const last = pTag[pTag.length - 1];
+    const lastlength = last.textContent.length;
+    if (lastlength <= 0) {
+      console.log('p will be removed');
+      console.log(lastlength);
+      last.remove();
+      console.log('getdata', getData);
+      const iframe = document.createElement('iframe');
+      document.body.appendChild(iframe);
+      iframe.contentWindow.document.open();
+      iframe.contentWindow.document.write('Hello world');
+      iframe.contentWindow.document.close();
+      console.log(iframe);
+    } else {
+      console.log('p do not removed');
+      console.log(lastlength);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <textarea
+        rows='15'
+        cols='100'
+        placeholder='enter data here'
+        onChange={handleChange}
+      />
+      <button onClick={handleSubmit}>click</button>
+      {/* <iframe width='33%' height='150px'>
+        {show && <div>{html}</div>}
+      </iframe> */}
+    </>
   );
 }
 
